44d311c830485f5dc9612c47b152cff0faead2fa
Edwin Martinez <dev.edwinmartinez@gmail.com>
2018-12-17 11:58:47 +0800

    Add listener to error state

--- borderedstyleview/src/main/java/com/github/edwnmrtz/borderedstyleedittext/NormalBorderedStyleEditText.kt
+++ borderedstyleview/src/main/java/com/github/edwnmrtz/borderedstyleedittext/NormalBorderedStyleEditText.kt
@@ -7,7 +7,9 @@ import android.support.constraint.ConstraintLayout
 import android.support.v4.content.ContextCompat
 import android.support.v7.widget.AppCompatEditText
 import android.support.v7.widget.AppCompatTextView
+import android.text.Editable
 import android.text.InputFilter
+import android.text.TextWatcher
 import android.util.AttributeSet
 import android.util.Log
 import android.view.View
@@ -16,20 +18,19 @@ import android.view.inputmethod.EditorInfo
 
 class NormalBorderedStyleEditText (context: Context, attrs: AttributeSet) : ConstraintLayout(context, attrs) {
 
-    private lateinit var tvTitle: AppCompatTextView
-    private lateinit var tvError: AppCompatTextView
-    private lateinit var etField: AppCompatEditText
+    private var tvTitle: AppCompatTextView
+    private var tvError: AppCompatTextView
+    private var etField: AppCompatEditText
 
-    private var titleTextColor = Color.BLACK
-    private var errorTextColor: Int = Color.RED
+    private var titleTextColor : Int    = Color.BLACK
+    private var errorTextColor: Int     = Color.RED
+
+    private var isError = false
 
     private val attributes: TypedArray = context.theme.obtainStyledAttributes(attrs, R.styleable.NormalBorderedStyleEditText, 0, 0)
 
-
-
     init {
         View.inflate(context, R.layout.bordered_edittext_normal, this)
-
         tvTitle = findViewById(R.id.tvTitle)
         tvError = findViewById(R.id.tvErrorText)
         etField = findViewById(R.id.etField)
@@ -77,9 +78,32 @@ class NormalBorderedStyleEditText (context: Context, attrs: AttributeSet) : Cons
             }
         }
         attributes.recycle()
+
+        textChangeListener()
+    }
+
+    private fun textChangeListener() {
+        etField.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                if(isError) {
+                    removeError()
+                    isError = false
+                }
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                //Ignore
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                //Ignore
+            }
+
+        })
     }
 
     fun setError(errorMessage: String) {
+        isError = true
         tvTitle.setTextColor(Color.RED)
         tvError.visibility = View.VISIBLE
         tvError.setTextColor(Color.RED)
